3af16ff70f3e982803f4daf6093af0e8775b4c3d
thazsobral <thazsobral@localhost.localdomain>
2021-07-13 05:53:05 -0300

    renamed files in pages folder

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Repositories from "./pages/repositories/Repositories";
-import Home from "./pages/home/Home";
+import Repositories from "./pages/repositories";
+import Home from "./pages/home";
 
 export default function Routes() {
     return(
