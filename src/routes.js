1d845d56e820d8a8531bd7848ee3a2da6a7a3cfd
thazsobral <thazsobral@localhost.localdomain>
2021-07-13 06:25:46 -0300

    styled components

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Repositories from "./pages/repositories";
 import Home from "./pages/home";
+import "./style.css";
 
 export default function Routes() {
     return(
